4781c53274b5a0963db9cb6bd4f7d6cbfc3a0a7e
Soddik <sodd86@gmail.com>
2022-07-20 15:35:11 +0300

    refactoring: +inner exception classes

--- src/main/java/com/soddik/exception/CardAttributeException.java
+++ src/main/java/com/soddik/exception/CardAttributeException.java
@@ -1,7 +1,19 @@
 package com.soddik.exception;
 
-public class CardAttributeException extends RuntimeException{
+public class CardAttributeException extends RuntimeException {
     public CardAttributeException(String message) {
         super(message);
+    }
+
+    public static class UnexpectedCardAttributeValueException extends RuntimeException {
+        public UnexpectedCardAttributeValueException(String message) {
+            super(message);
+        }
+    }
+
+    public static class UnexpectedCardAttributeKindException extends RuntimeException {
+        public UnexpectedCardAttributeKindException(String message) {
+            super(message);
+        }
     }
 }
--- src/main/java/com/soddik/exception/UnexpectedCardAttribute.java
+++
@@ -1,7 +0,0 @@
-package com.soddik.exception;
-
-public class UnexpectedCardAttribute extends RuntimeException {
-    public UnexpectedCardAttribute(String message) {
-        super(message);
-    }
-}
